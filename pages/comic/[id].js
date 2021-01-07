7683a7e3d4c7a4694d1c42446410271381c4441a
Mark Bell <skrambelled@gmail.com>
2021-01-06 16:46:30 -0800

    added individual comics page

--- pages/comic/[id].js
+++ pages/comic/[id].js
@@ -1,2 +1,16 @@
+import ComicList from '../../components/ComicList'
 import Layout from '../../components/layout'
+import { fetch } from '../../services/fetcher'
 
+
+const Comic = ({ comic }) => (
+    <Layout>
+        <h2>{comic && comic.name}</h2>
+        <img
+            src={comic && comic.src}
+            alt={comic && comic.text}
+        />
+    </Layout>
+)
+
+export default Comic
--- services/fetcher.js
+++ services/fetcher.js
@@ -1,1 +1,17 @@
 import axios from 'axios'
+
+export async function fetch(id, web=false) {
+    if(web)
+        return webFetch(id)
+    else   
+        return mockFetch(id)
+}
+
+function mockFetch(id) {
+    
+}
+
+function webFetch(id) {
+    // TODO: build a web fetcher
+    return
+}
